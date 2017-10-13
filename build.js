932daa59a13a2c33a0b91442731bdd493b8d904a
Alex E Scott <aescott@outlook.com>
2017-10-13 15:26:40 -0700

    new resonse functions

--- build.js
+++ build.js
@@ -1,24 +1,55 @@
 // document.addEventListener("DOMContentLoaded", );
 let duderino = () => {
   let timeouts = [];
+  const assembleResponse = (title, definition) => {
+    title.className = "title";
+    let final = document.createElement('div');
+    final.appendChild(title);
+    final.appendChild(definition);
+    return final
+  }
+
+  const successResponse = (text) => {
+    let parser = new DOMParser();
+    let htmlDoc = parser.parseFromString(text, "text/html");
+    let divs = htmlDoc.querySelectorAll(".word--C9UPa");
+    if (divs.length === 1) {
+      let div = divs[0].firstChild;
+      let title = div.firstChild;
+      let definition = div.lastChild.lastChild;
+      return assembleResponse(title, definition);
+    } else {
+      let list = document.createElement('div');
+      for (let i = 0; i < divs.length; i++) {
+        let div = divs[i].firstChild;
+        let title = div.firstChild;
+        let definition = div.lastChild.lastChild;
+        list.appendChild(assembleResponse(title, definition));
+      }
+      return list;
+    }
+  }
+
+  const failResponse = () => {
+    let title = document.createElement('h1');
+    title.innerHTML = "Sorry! (interj.)";
+    let definition = document.createElement('object');
+    definition.innerHTML = "The word you're looking for cannot be found :(";
+
+    return assembleResponse(title, definition);
+  }
+
   const getEtym = async word => {
     const proxyurl = "https://yes-proxy.herokuapp.com/";
-    // "http://localhost:8080/"
-    //  "https://cors-anywhere.herokuapp.com/";
-    let url = `http://www.etymonline.com/word${word}`;
+    let url = `http://www.etymonline.com/word/${word}`;
 
-    const data = await fetch(proxyurl + url);
-    const words = await data.text();
-    let parser = new DOMParser();
-    let htmlDoc = parser.parseFromString(words, "text/html");
-    let html = htmlDoc.getElementById("dictionary");
-    let links = html.querySelectorAll("a");
-    links.forEach(el => {
-      let oldHref = el.href;
-      let index = /index/.exec(oldHref).index;
-      el.href = `http://www.etymonline.com/${oldHref.slice(index)}`;
-    })
-    return html;
+    let response = await fetch(proxyurl + url);
+    if (response.ok) {
+      let text = await response.text();
+      return successResponse(text);
+    } else {
+      return failResponse();
+    }
   };
 
   let closeTimer = () => {
--- rebuild.js
+++ rebuild.js
@@ -1,3 +1,63 @@
+const assembleResponse = (title, definition) => {
+  title.className = "title";
+  let final = document.createElement('div');
+  final.appendChild(title);
+  final.appendChild(definition);
+  return final
+}
+
+const successResponse = (text) => {
+  let parser = new DOMParser();
+  let htmlDoc = parser.parseFromString(text, "text/html");
+  let divs = htmlDoc.querySelectorAll(".word--C9UPa");
+  if (divs.length === 1) {
+    let div = divs[0].firstChild;
+    let title = div.firstChild;
+    let definition = div.lastChild.lastChild;
+    return assembleResponse(title, definition);
+  } else {
+    let list = document.createElement('div');
+    for (let i = 0; i < divs.length; i++) {
+      let div = divs[i].firstChild;
+      let title = div.firstChild;
+      let definition = div.lastChild.lastChild;
+      list.appendChild(assembleResponse(title, definition));
+    }
+    return list;
+  }
+}
+
+const failResponse = () => {
+  let title = document.createElement('h1');
+  title.innerHTML = "Sorry! (interj.)";
+  let definition = document.createElement('object');
+  definition.innerHTML = "The word you're looking for cannot be found :(";
+
+  return assembleResponse(title, definition);
+}
+
+const getEtym = async word => {
+  const proxyurl = "https://yes-proxy.herokuapp.com/";
+  let url = `http://www.etymonline.com/word/${word}`;
+
+  let response = await fetch(proxyurl + url);
+  if (response.ok) {
+    let text = await response.text();
+    return successResponse(text);
+  } else {
+    return failResponse();
+  }
+ }
+
+
+ // let links = html.querySelectorAll("a");
+ // links.forEach(el => {
+ //   let oldHref = el.href;
+ //   let index = /index/.exec(oldHref).index;
+ //   el.href = `http://www.etymonline.com/${oldHref.slice(index)}`;
+ // })
+
+
 
 const myParse = data => {
   data.text().then(words => {
@@ -21,15 +81,5 @@ const myFetch = async word => {
     }
   })
 }
-
-const getEtym = async word => {
-  try {
-    const data = await myFetch(word);
-    if(data) return myParse(data);
-  }
-  catch(e) {
-    console.log(e);
-  }
-};
 
 getEtym("capybara").then(r => console.log(r))
