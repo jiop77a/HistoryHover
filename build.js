6e1d3ae8048c6e42898f538f68d664ed1e684d55
Alex E Scott <aescott@outlook.com>
2017-10-17 12:54:06 -0700

    standardize result div

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 let duderino = () => {
   let timeouts = [];
+
   const assembleResponse = (title, definition) => {
     title.className = "title";
     let final = document.createElement('div');
@@ -12,21 +13,14 @@ let duderino = () => {
     let parser = new DOMParser();
     let htmlDoc = parser.parseFromString(text, "text/html");
     let divs = htmlDoc.querySelectorAll(".word--C9UPa");
-    if (divs.length === 1) {
-      let div = divs[0].firstChild;
+    let list = document.createElement('div');
+    for (let i = 0; i < divs.length; i++) {
+      let div = divs[i].firstChild;
       let title = div.firstChild;
       let definition = div.lastChild.lastChild;
-      return assembleResponse(title, definition);
-    } else {
-      let list = document.createElement('div');
-      for (let i = 0; i < divs.length; i++) {
-        let div = divs[i].firstChild;
-        let title = div.firstChild;
-        let definition = div.lastChild.lastChild;
-        list.appendChild(assembleResponse(title, definition));
-      }
-      return list;
+      list.appendChild(assembleResponse(title, definition));
     }
+    return list;
   }
 
   const failResponse = () => {
